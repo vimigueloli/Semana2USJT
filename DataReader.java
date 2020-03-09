a2844eeee0917ed829f132bb4a43886ef227ae64
vimiugeloli <vipeso2016@gmail.com>
2020-03-09 15:23:16 -0300

    'alterado'

--- DataReader.java
+++ DataReader.java
@@ -1,13 +1,10 @@
 import javax.swing.JOptionPane;
 
 public class DataReader{
-   
    public static void main(String [] args){
-   
       int day = Integer.parseInt(JOptionPane.showInputDialog("digite o dia"));
       int month = Integer.parseInt(JOptionPane.showInputDialog("digite o mês"));
-      int year = Integer.parseInt(JOptionPane.showInputDialog("digite o ano"));
-      
+      int year = Integer.parseInt(JOptionPane.showInputDialog("digite o ano"));      
       if(day<32){
          if(month<13){
             JOptionPane.showMessageDialog(null,day + "/" + month + "/" + year);   
